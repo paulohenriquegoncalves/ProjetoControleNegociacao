90114686ccbd513f7f5a296c374fb03c90b2060b
Paulo Henrique <paulohenriquegoncalves@gmail.com>
2022-03-23 15:13:36 -0300

    Retirando classe de exemplo + alterações no cadastro de distribuidor

--- src/View/uView.CadastroDistribuidor.dfm
+++ src/View/uView.CadastroDistribuidor.dfm
@@ -1,5 +1,66 @@
 inherited frmViewCadastroDistribuidor: TfrmViewCadastroDistribuidor
-  Caption = 'frmViewCadastroDistribuidor'
+  Caption = 'Distribuidor'
+  ClientHeight = 93
+  OnShow = FormShow
+  ExplicitHeight = 132
   PixelsPerInch = 96
   TextHeight = 13
+  inherited pnlCadastro: TPanel
+    Height = 56
+    object Label11: TLabel
+      Left = 10
+      Top = 5
+      Width = 37
+      Height = 13
+      Alignment = taRightJustify
+      Caption = 'C'#243'digo:'
+    end
+    object Label1: TLabel
+      Left = 79
+      Top = 5
+      Width = 58
+      Height = 13
+      Alignment = taRightJustify
+      Caption = 'Distribuidor:'
+    end
+    object Label2: TLabel
+      Left = 428
+      Top = 5
+      Width = 29
+      Height = 13
+      Alignment = taRightJustify
+      Caption = 'CNPJ:'
+    end
+    object edtCodigoDistribuidor: TEdit
+      Left = 10
+      Top = 24
+      Width = 59
+      Height = 21
+      Color = clWhite
+      Enabled = False
+      ReadOnly = True
+      TabOrder = 0
+    end
+    object edtNomeDistribuidor: TEdit
+      Left = 79
+      Top = 24
+      Width = 343
+      Height = 21
+      MaxLength = 100
+      TabOrder = 1
+    end
+    object mskEdtCNPJ: TMaskEdit
+      Left = 428
+      Top = 24
+      Width = 247
+      Height = 21
+      EditMask = '!99.999.999/9999-99;0;'
+      MaxLength = 18
+      TabOrder = 2
+      Text = ''
+    end
+  end
+  inherited pnlBotores: TPanel
+    Top = 56
+  end
 end
--- src/View/uView.CadastroDistribuidor.pas
+++ src/View/uView.CadastroDistribuidor.pas
@@ -5,10 +5,18 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, uView.CadastroPadrao, Vcl.StdCtrls,
-  Vcl.ExtCtrls;
+  Vcl.ExtCtrls, Vcl.Mask;
 
 type
   TfrmViewCadastroDistribuidor = class(TfrmViewCadastroPadrao)
+    Label11: TLabel;
+    edtCodigoDistribuidor: TEdit;
+    edtNomeDistribuidor: TEdit;
+    Label1: TLabel;
+    mskEdtCNPJ: TMaskEdit;
+    Label2: TLabel;
+    procedure FormShow(Sender: TObject);
+    procedure btnSalvarClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -21,5 +29,57 @@ var
 implementation
 
 {$R *.dfm}
+uses uView.PesquisaDistribuidor, uExcessoes, uTypes;
+
+
+procedure TfrmViewCadastroDistribuidor.btnSalvarClick(Sender: TObject);
+begin
+  inherited;
+  try
+    frmViewPesquisaDistribuidor.controllerDistribuidor.ModelDistribuidor.TipoOperacao        := frmViewPesquisaDistribuidor.TipoOperacao;
+    frmViewPesquisaDistribuidor.controllerDistribuidor.ModelDistribuidor.CodigoDistribuidor  := StrToIntDef(edtCodigoDistribuidor.Text,0);
+    frmViewPesquisaDistribuidor.controllerDistribuidor.ModelDistribuidor.NomeDistribuidor    := edtNomeDistribuidor.text;
+    frmViewPesquisaDistribuidor.controllerDistribuidor.ModelDistribuidor.CNPJ                := mskEdtCNPJ.Text;
+
+    if frmViewPesquisaProduto.controllerProduto.persistir then
+       showmessage('Operação realizada com sucesso!')
+     else
+       showmessage('Não foi possível realizar a operação!')
+  except
+    on E: ENomeProdutoObrigatorio do
+    begin
+      edtNomeProduto.SetFocus;
+      Raise;
+    end;
+
+    on E: EPrecoRevendaInvalido do
+    begin
+      edtPrecoVenda.SetFocus;
+      Raise;
+    end;
+
+    on E: Exception do
+    begin
+      raise Exception.Create('Não foi possível realizar a operação!'+E.Message);
+    end;
+  end;
+end;
+
+procedure TfrmViewCadastroDistribuidor.FormShow(Sender: TObject);
+begin
+  inherited;
+  if frmViewPesquisaDistribuidor.TipoOperacao = tpInclusao then
+  begin
+    edtCodigoDistribuidor.Clear;
+    edtNomeDistribuidor.Clear;
+    mskEdtCNPJ.Clear;
+  end
+  else
+  begin
+    edtCodigoDistribuidor.Text := frmViewPesquisaDistribuidor.memTabela.FieldByName('CODDISTRIBUIDOR').AsInteger.ToString;
+    edtNomeDistribuidor.Text   := frmViewPesquisaDistribuidor.memTabela.FieldByName('NOMEDISTRIBUIDOR').AsString;
+    mskEdtCNPJ.Text            := frmViewPesquisaDistribuidor.memTabela.FieldByName('CNPJ').AsString;
+  end;
+end;
 
 end.
