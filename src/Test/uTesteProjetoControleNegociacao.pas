30281e7208d6ee2fbffa9b42fcce494771120fc8
Paulo Henrique <paulohenriquegoncalves@gmail.com>
2022-03-31 12:29:48 -0300

    imagens para documentacao

--- src/Test/uTesteProjetoControleNegociacao.pas
+++ src/Test/uTesteProjetoControleNegociacao.pas
@@ -26,13 +26,12 @@ type
     [Test]
     procedure TestProdutorCNPJInvalido;
 
-    // Test with TestCase Attribute to supply parameters.
-{
+
     [Test]
     [TestCase('TestA','1,2')]
     [TestCase('TestB','3,4')]
     procedure Test2(const AValue1 : Integer;const AValue2 : Integer);
-}
+
   end;
 
 implementation
@@ -77,11 +76,12 @@ begin
 end;
 
 
-{
+
 procedure TMyTestObject.Test2(const AValue1 : Integer;const AValue2 : Integer);
 begin
+
 end;
-}
+
 initialization
   TDUnitX.RegisterTestFixture(TMyTestObject);
 
